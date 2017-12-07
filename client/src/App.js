8da7ff1e57bfc47dfd83f1e6063ca4a4530d6645
JazyLabs <yassinej@gmail.com>
2017-12-07 20:20:30 +0200

    added react-router and single page navigation with browser history

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,28 @@
 import React, { Component } from 'react';
-import logo from './assets/logo.svg';
-import './assets/App.css';
+import { Router, Route, withRouter} from 'react-router-dom';
+import history from './services/history';
+import NavBar from './NavBar.js';
+import Footer from './Footer.js';
+import Landing from './Landing.js';
+import Dashboard from './Dashboard.js';
+import GetStarted from './GetStarted.js';
 
+//Allows NavBar to access Location. Useful for Active class
+const LocatedNavBar = withRouter(NavBar);
+
+//The exact parameter in Route avoids that all routes starting with / are sent to Landing
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to DashTings.io</h1>
-        </header>
-        <p className="App-intro">
-          <strong>Let's Get Started Team Geckos-14!</strong>
-        </p>
-      </div>
+      <Router history={history}>
+				<div className="container-fluid p-0">
+					<LocatedNavBar />
+					<Route exact path="/" component={Landing} />
+					<Route path="/getstarted" component={GetStarted} />
+					<Route path="/dashboard" component={Dashboard} />
+					<Footer />
+				</div>
+			</Router>
     );
   }
 }
--- client/src/Landing.js
+++ client/src/Landing.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
+import logo from './assets/logo.svg';
+import './assets/App.css';
 
 class Landing extends Component {
   render() {
     return (
-      <div>Landing Page</div>
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to DashTings.io</h1>
+          </header>
+          <p className="App-intro">
+            <strong>Let's Get Started Team Geckos-14!</strong>
+          </p>
+        </div>
     );
   }
 }
