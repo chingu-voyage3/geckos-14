66e4656f1128a2198a16329db27793d6bacd3300
JazyLabs <yassinej@gmail.com>
2017-12-08 19:51:54 +0200

    Added testSetup file to fix test issues

--- client/src/tests/NavBar.test.js
+++ client/src/tests/NavBar.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import NavBar from '../NavBar';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
