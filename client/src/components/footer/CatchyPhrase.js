f8871367138d61bcb895d3642bedfb9ee33aab87
mmckalan <mmckalan@gmail.com>
2017-12-16 14:38:32 +0100

    Footer 1st Commit

---
+++ client/src/components/footer/CatchyPhrase.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CatchyPhrase = () => {
+   return (
+        <div className="catchyPhrase">
+          <p><strong>DashThings.io - An easy way to build <a href="#toDashboards">Dashboards</a> for your things.</strong></p>
+        </div>
+    );
+};
+
+export default CatchyPhrase;
---
+++ client/src/components/footer/SupportMail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const SupportMail = () => {
+   return (
+       <div className="supportMail">
+        <i className="fa fa-envelope fa-x2"></i><a href="#toContact">support@dashthings.io</a>
+
+        </div>
+    );
+};
+
+  export default SupportMail;
