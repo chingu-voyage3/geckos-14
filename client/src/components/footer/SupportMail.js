b3a47fc0a5d3b6703ddc88c5cbcdbe26df8e7fe5
mmckalan <mmckalan@gmail.com>
2017-12-16 15:38:58 +0100

    Fixing link tag

--- client/src/components/footer/SupportMail.js
+++ client/src/components/footer/SupportMail.js
@@ -3,7 +3,7 @@ import React from 'react';
 const SupportMail = () => {
    return (
        <div className="footer-supportMail">
-        <i className="fa fa-envelope fa-x2"></i><link href="#toContact">support@dashthings.io</link>
+        <i className="fa fa-envelope fa-x2"></i><link to="/contact">support@dashthings.io</link>
 
         </div>
     );
