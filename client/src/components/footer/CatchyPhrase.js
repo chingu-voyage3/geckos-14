55a438b8d7f351d4f7fed91b63ca3dc5ccb51d4a
mmckalan <mmckalan@gmail.com>
2017-12-18 00:33:45 +0100

    Fixed Dashboard Link

--- client/src/components/footer/CatchyPhrase.js
+++ client/src/components/footer/CatchyPhrase.js
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 const CatchyPhrase = () => {
    return (
         <div className="footer-catchyphrase">
-          <p><strong>DashThings.io - An easy way to build <Link to="/Dashboards">Dashboards</Link> for your things.</strong></p>
+          <p><strong>DashThings.io - An easy way to build <Link to="/dashboards">Dashboards</Link> for your things.</strong></p>
         </div>
     );
 };
