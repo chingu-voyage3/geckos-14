ef1fb81aec6234ed691483ec2209c4346bcc7382
JazyLabs <yassinej@gmail.com>
2017-12-18 18:01:46 +0200

    changed url to /dashboard in CatchyPhrase

--- client/src/components/footer/CatchyPhrase.js
+++ client/src/components/footer/CatchyPhrase.js
@@ -2,11 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const CatchyPhrase = () => {
-   return (
-        <div className="footer-catchyphrase">
-          <p><strong>DashThings.io - An easy way to build <Link to="dashboard">Dashboards</Link> for your things.</strong></p>
-        </div>
-    );
+  return (
+    <div className="footer-catchyphrase">
+      <p>
+        <strong>
+          DashThings.io - An easy way to build <Link to="/dashboard">Dashboards</Link> for your
+          things.
+        </strong>
+      </p>
+    </div>
+  );
 };
 
 export default CatchyPhrase;
