a6de8e8a6841c7c69f42c208708544c6cbf128f4
JazyLabs <yassinej@gmail.com>
2017-12-27 23:29:17 +0200

    Added discovery based on WoT Online Pi device and WoT Protocol

--- client/src/helpers/discover.js
+++ client/src/helpers/discover.js
@@ -1,3 +1,30 @@
+import axios from 'axios';
+import * as d from '../tempData.js';
+
+function checkResources(res, url) {
+  if (res.data.resources) {
+    const resources = res.data.resources;
+    for (let key in resources) {
+      let resUrl = url + resources[key].url;
+      discover(resUrl);
+    }
+  } else {
+    console.log('to Add to DeviceList', res.data);
+    let devices = res.data;
+    for (let key in devices) {
+      d.devices.push(devices[key]);
+    }
+  }
+}
 export default function discover(url) {
   console.log('Discovering Devices from ', url);
+  axios
+    .get(url)
+    .catch(err => {
+      console.log('Error Discovering Root...', err);
+    })
+    .then(res => {
+      console.log('Discovered :', res.data);
+      checkResources(res, url);
+    });
 }
