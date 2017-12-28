63feabff2d40345472f9d343a960e67f2a86605b
JazyLabs <yassinej@gmail.com>
2017-12-28 23:30:39 +0200

    Generating Device List with ID

--- client/src/components/dashboard/DeviceList.js
+++ client/src/components/dashboard/DeviceList.js
@@ -11,7 +11,7 @@ const renderDevice = (device, actions) => {
   return (
     <div
       className="device"
-      key={device.name}
+      key={device.id}
       onClick={() => {
         actions.toogleSelectedDevice(device.id);
       }}
--- client/src/helpers/discover.js
+++ client/src/helpers/discover.js
@@ -3,6 +3,12 @@ import * as d from '../tempData.js';
 
 let devices = [];
 
+function formatDevice(device, listLength, url) {
+  let newDevice = { id: 'dev' + listLength, name: device.name, url: url };
+  console.log(newDevice);
+  return newDevice;
+}
+
 export function getDevices(url, devList) {
   // console.log('Discovering Actuators from ', resUrl);
   return axios
@@ -14,7 +20,8 @@ export function getDevices(url, devList) {
       const devs = res.data;
       for (let k in devs) {
         // console.log(devs[k]);
-        devList.push(devs[k]);
+        let device = formatDevice(devs[k], devList.length, url);
+        devList.push(device);
       }
       return devList;
     });
