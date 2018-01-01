12f4ba341b040f98ebbb812d98011be14f7104cf
JazyLabs <yassinej@gmail.com>
2018-01-01 23:21:25 +0200

    Added Basic LoggedIn logic

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -3,22 +3,64 @@ import { Link } from 'react-router-dom';
 import './assets/NavBar.css';
 
 class NavBar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLogged: false
+    };
+  }
+  handleLogin = () => {
+    if (!this.state.isLogged) {
+      this.setState({ isLogged: true });
+    }
+  };
+  handleLogout = () => {
+    if (this.state.isLogged) {
+      this.setState({ isLogged: false });
+    }
+  };
+  renderLogin() {
+    if (this.state.isLogged) {
+      return (
+        <ul>
+          <li>
+            <Link to="/profile">Profile</Link>
+          </li>
+          <li>
+            <button onClick={this.handleLogout}>LogOut</button>
+          </li>
+        </ul>
+      );
+    }
+
+    return (
+      <ul>
+        <li>
+          <button onClick={this.handleLogin}>LogIn</button>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul>
+    );
+  }
   render() {
     return (
-        <div className="nav-bar">
-          <ul>
-            <li>DashThing Logo</li>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/getstarted">Get Started</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </div>
+      <div className="nav-bar">
+        <ul>
+          <li>DashThing Logo</li>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/getstarted">Get Started</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        {this.renderLogin()}
+      </div>
     );
   }
 }
