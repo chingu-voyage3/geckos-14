11eebef6c46feb83d1bec12b12f433e92f13ac40
JazyLabs <yassinej@gmail.com>
2018-01-13 17:02:35 +0200

    Fixed websocket integration with switch

--- client/src/components/dashboard/Switch.js
+++ client/src/components/dashboard/Switch.js
@@ -1,25 +1,61 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Switch = props => {
-  return Object.keys(props.values).map(value => {
-    let status = props.values[value];
-    let data = props.data[Object.keys(props.data).length - 1];
-
-    return (
-      <div key={'status-' + status.name}>
-        <label className="switch-name">{status.name}</label>
-        <div>
-          <span className="switch-info-false">False</span>
-          <label className="switch">
-            <input type="checkbox" checked={data.value} />
-            <span className="switch-slider round" />
-          </label>
-          <span className="switch-info-true">True</span>
+class Switch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: '',
+      name: '',
+      switches: []
+    };
+  }
+  componentWillMount() {
+    // console.log('SwitchList props:', this.props);
+    this.setState({
+      id: 'switch-' + this.props.source_id,
+      name: this.props.name,
+      switches: this.getSwitches(this.props)
+    });
+    // populate initial state using props
+  }
+  componentWillReceiveProps(nextProps) {
+    // check nextProps for change in checked status
+    this.setState({
+      switches: this.getSwitches(nextProps)
+    });
+  }
+  getSwitches = props => {
+    const switches = [];
+    Object.keys(props.values).forEach(value => {
+      let sw = props.values[value];
+      let d = props.data[Object.keys(props.data).length - 1];
+      let state = d ? d[value] : 'Awaiting';
+      let time = d ? d.timestamp : new Date().toISOString();
+      switches.push({
+        name: sw.name,
+        time: time,
+        state: state
+      });
+    });
+    return switches;
+  };
+  renderSwitchList = () => {
+    return this.state.switches.map(sw => {
+      return (
+        <div key={sw.name} className="switch">
+          <div className="switch-name">{sw.name + ' - ' + sw.time}</div>
+          <div className="switch-label">
+            <label>{sw.state ? 'ON' : 'OFF'}</label>
+          </div>
+          <div className="switch-time" />
         </div>
-        <span className="">{data.timestamp}</span>
-      </div>
-    );
-  });
-};
+      );
+    });
+  };
+
+  render() {
+    return <div>{this.renderSwitchList()}</div>;
+  }
+}
 
 export default Switch;
