ef5ab327c2445653214a22242126619b48f69daf
JazyLabs <yassinej@gmail.com>
2018-01-14 16:34:24 +0200

    added checkViz and messages for error and success

--- client/src/containers/Dashboard.js
+++ client/src/containers/Dashboard.js
@@ -146,9 +146,9 @@ class Dashboard extends Component {
     }
   };
   delViz = viz => {
-    console.log('Deleting viz...', viz.id);
+    // console.log('Deleting viz...', viz.id);
     if (viz.dataType === 'ws') {
-      console.log('Closing Socket...');
+      // console.log('Closing Socket...');
       viz.socket.close();
     }
     this.setState({
@@ -157,9 +157,12 @@ class Dashboard extends Component {
     });
   };
   checkViz = viz => {
-    let valid = true;
-
-    return valid;
+    if (viz.name && viz.dataType && viz.vizType && viz.design && viz.source_id && viz.model) {
+      // console.log('Proper Viz');
+      return true;
+    }
+    // console.log('Bad Viz');
+    return false;
   };
   checkUrl = url => {
     const regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
